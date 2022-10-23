b28a69db7976bc649b90544803deb54c0445e128
Nikodem Wrona <wronanikodem@protonmail.com>
2022-10-23 19:54:10 +0200

    feat: v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # check-currency-rates
-Very simple script for fetching currency rates from NBP api.
+Very simple script for checking currency rates using NBP api.
+
+## Usage
+Clone the script to you machine and run:
+
+```shell
+python main.py
+```
+
+It will by default display a rate for EUR/PLN pair.
+
+To check different symbol like GBP, run following: 
+
+```shell
+python main.py gbp
+```
---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+import json
+import sys
+
+
+def get_euro_to_pln_exchange_rate(currency='eur'):
+    """Fetches current rate of given currency to PLN from NBP.
+
+    Keyword arguments
+        currency -- Symbol of currency to fetch the rate for (default = "eur")
+    """
+    result = requests.get(f'http://api.nbp.pl/api/exchangerates/rates/a/{currency}/'.format(currency=currency))
+    data = json.loads(result.text)
+    rate = data["rates"][0]["mid"]
+
+    print(f'{currency.upper()}/PLN rate : {rate}')
+
+
+if len(sys.argv) > 1:
+    currencySymbol = sys.argv[1]
+    get_euro_to_pln_exchange_rate(currency=currencySymbol)
+else:
+    get_euro_to_pln_exchange_rate()
+
+
